70972eb32a43ad659b681d38f7b9eff483b52455
Johnsomi <mj4100@students.inghamisd.org>
2019-03-13 13:57:13 -0400

    13:30 #1

---
+++ InsectShooter/InsectShooter/Source/World.cs
@@ -0,0 +1,36 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace InsectShooter
+{
+    public class World
+    {
+        public World()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
